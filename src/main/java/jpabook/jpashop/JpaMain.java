2671686d9518b1cd845757f56aa058702968658b
psjw <dlrudtn108@gmail.com>
2025-03-31 17:55:26 +0900

    Inheritance

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -5,6 +5,7 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.EntityTransaction;
 import jakarta.persistence.Persistence;
 import jpabook.jpashop.domain.Book;
+import jpabook.jpashop.domain.Item;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
 
@@ -28,6 +29,10 @@ public class JpaMain {
             book.setName("JPA");
             book.setAuthor("kkk");
             em.persist(book);
+
+            em.createQuery("select i from Item i where type(i) = Book", Item.class)
+                    .getResultList();
+
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
